9f5882881ea83853777377c4e6167f218f2aab1a
JanTkacSK <jan.tkac.sk@gmail.com>
2025-07-02 15:27:20 +0200

    commit

---
+++ Public/Enable-ZXTrigger.ps1
@@ -0,0 +1,80 @@
+function Enable-ZXTrigger{
+    param(
+        [string]$TriggerId,
+        [switch]$WhatIf,
+        [switch]$ShowJsonRequest,
+        [switch]$ShowJsonResponse
+    )
+    #Verify parameters
+    if ($HostName -and $HostId){
+        Write-Host -ForegroundColor Yellow 'Not allowed to use -HostName and -HostID parameters together'
+        continue
+    }
+    
+    #Funcions
+    #A function that formats and displays the json request that is used in the API call, it removes the API token value and replaces it with *****
+    function ShowJsonRequest {
+        Write-Host -ForegroundColor Yellow "JSON REQUEST"
+        $PSObjShow = $PSObj
+        $PSObjShow.auth = "*****"
+        $JsonShow = $PSObjShow | ConvertTo-Json -Depth 5
+        Write-Host -ForegroundColor Cyan $JsonShow
+    } 
+
+    #Basic PS Object wich will be edited based on the used parameters and finally converted to json
+    $PSObj  = [PSCustomObject]@{
+        "jsonrpc" = "2.0"; 
+        "method" = "trigger.update"; 
+        "params" = [PSCustomObject]@{
+            "triggerid" = $TriggerId;
+            "status" = "0"
+        }; 
+        "auth" = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR(($Global:ZXAPIToken))); #This is the same as $Global:ZXAPIToken | ConvertFrom-SecureString -AsPlainText but this worsk also for PS 5.1
+        "id" = "1"
+    }
+
+    switch ($Status) {
+        "Enabled" {$Status = "0"}
+        "Disabled" {$Status = "1"}
+    }
+
+
+    $Json = $PSObj | ConvertTo-Json -Depth 5
+
+
+    #Show JSON Request if -ShowJsonRequest switch is used
+    If ($ShowJsonRequest -or $WhatIf){
+        Write-Host -ForegroundColor Yellow "JSON REQUEST"
+        $PSObjShow = $PSObj
+        $PSObjShow.auth = "*****"
+        $JsonShow = $PSObjShow | ConvertTo-Json -Depth 5
+        Write-Host -ForegroundColor Cyan $JsonShow
+    }
+    
+    #Make the API call
+    if(!$Whatif){
+        $Request = Invoke-RestMethod -Uri $ZXAPIUrl -Body $Json -ContentType "application/json" -Method Post
+    }
+
+    If ($ShowJsonResponse){
+        Write-Host -ForegroundColor Yellow "JSON RESPONSE"
+        Write-Host -ForegroundColor Cyan $($request | ConvertTo-Json -Depth 5)
+    }
+
+    #This will be returned by the function
+    if($null -ne $Request.error){
+        $Request.error
+        return
+    } 
+    elseif ($null -ne $Request.result) {
+        $Request.result
+        return
+    }
+    elseif(!$WhatIf) {
+        Write-Host -ForegroundColor Yellow "No result"
+        return
+    }
+    
+}
+
+
