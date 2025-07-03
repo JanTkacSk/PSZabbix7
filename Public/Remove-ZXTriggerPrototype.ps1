ae91db720bb4b31127cc0fbe60b0fac0d5cc5331
JanTkacSK <jan.tkac.sk@gmail.com>
2025-07-03 08:52:05 +0200

    commit

---
+++ Public/Remove-ZXTriggerPrototype.ps1
@@ -0,0 +1,68 @@
+function Remove-ZXTriggerPrototype{
+    param(
+        [array]$TriggerPrototypeID,
+        [switch]$WhatIf,
+        [switch]$ShowJsonRequest,
+        [switch]$ShowJsonResponse
+    )
+   
+    #Funcions
+    #A function that formats and displays the json request that is used in the API call, it removes the API token value and replaces it with *****
+    function ShowJsonRequest {
+        Write-Host -ForegroundColor Yellow "JSON REQUEST"
+        $PSObjShow = $PSObj
+        $PSObjShow.auth = "*****"
+        $JsonShow = $PSObjShow | ConvertTo-Json -Depth 5
+        Write-Host -ForegroundColor Cyan $JsonShow
+    } 
+
+    #Basic PS Object wich will be edited based on the used parameters and finally converted to json
+    $PSObj  = [PSCustomObject]@{
+        "jsonrpc" = "2.0"; 
+        "method" = "triggerprototype.delete"; 
+        "params" = $TriggerPrototypeID; 
+        "auth" = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR(($Global:ZXAPIToken))); #This is the same as $Global:ZXAPIToken | ConvertFrom-SecureString -AsPlainText but this worsk also for PS 5.1
+        "id" = "1"
+    }
+
+
+    $Json = $PSObj | ConvertTo-Json -Depth 5
+
+
+    #Show JSON Request if -ShowJsonRequest switch is used
+    If ($ShowJsonRequest -or $WhatIf){
+        Write-Host -ForegroundColor Yellow "JSON REQUEST"
+        #Make a clean copy of an object - have to uset convertto and convertfrom in order to break the references to the original object
+        $PSObjShow = $PSObj | ConvertTo-Json | ConvertFrom-Json
+        $PSObjShow.auth = "*****"
+        $JsonShow = $PSObjShow | ConvertTo-Json -Depth 5
+        Write-Host -ForegroundColor Cyan $JsonShow
+    }
+    
+    #Make the API call
+    if(!$Whatif){
+        $Request = Invoke-RestMethod -Uri $ZXAPIUrl -Body $Json -ContentType "application/json" -Method Post
+    }
+
+    If ($ShowJsonResponse){
+        Write-Host -ForegroundColor Yellow "JSON RESPONSE"
+        Write-Host -ForegroundColor Cyan $($request | ConvertTo-Json -Depth 5)
+    }
+
+    #This will be returned by the function
+    if($null -ne $Request.error){
+        $Request.error
+        return
+    } 
+    elseif ($null -ne $Request.result) {
+        $Request.result
+        return
+    }
+    elseif(!$WhatIf) {
+        Write-Host -ForegroundColor Yellow "No result"
+        return
+    }
+    
+}
+
+
