f719f7f49bc080266e95b0fadddcc88fd90ef035
JanTkacSK <jan.tkac.sk@gmail.com>
2025-09-10 14:51:08 +0200

    commit

--- PSZabbix7.psm1
+++ PSZabbix7.psm1
@@ -669,6 +669,74 @@ class ZXTagList {
     }
 }
 
+function Copy-ZXHostParameters {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$name,
+
+        [Parameter(Mandatory=$true)]
+        [string]$sampleHost,
+
+        [string]$alias,
+        [string]$newIp,
+        [string]$newDns
+    )
+
+    # Get host data
+    $hostData = Get-ZXHost -name $sampleHost -IncludeTags -IncludeMacros -IncludeParentTemplates -IncludeInterfaces -IncludeHostGroups -InterfaceProperties extend | ConvertTo-Json -Depth 5 | ConvertFrom-Json -Depth 5
+
+    if(!$hostData){
+        Write-Host -ForegroundColor Yellow "Sample host not found, use a name that exactly matches the host name"
+        return
+    }
+
+    # Remove unwanted properties
+    $hostData.PSObject.Properties.Remove("hostid")
+    $hostData.groups | ForEach-Object { $_.PSObject.Properties.Remove("name") }
+    $hostData.parentTemplates | ForEach-Object { $_.PSObject.Properties.Remove("name") }
+    $hostData.interfaces | ForEach-Object {
+        $_.PSObject.Properties.Remove("available")
+        $_.PSObject.Properties.Remove("error")
+        $_.PSObject.Properties.Remove("errors_from")
+        $_.PSObject.Properties.Remove("disable_until")
+    }
+
+    # Clean up macros: keep only macro and value
+    $cleanMacros = @()
+    foreach ($macro in $hostData.macros) {
+        $cleanMacros += @{
+            macro = $macro.macro
+            value = $macro.value
+        }
+    }
+
+    # Rename ParentTemplates to templates
+    $hostData | Add-Member -MemberType NoteProperty -Name templates -Value $hostData.parentTemplates
+    $hostData.PSObject.Properties.Remove("parentTemplates")
+
+    # Replace host and name
+    $hostData.host = $name
+    $hostData.name = if ($alias) { $alias } else { $name }
+
+    # Update IP and DNS if provided
+    if ($newIp) { $hostData.interfaces[0].ip = $newIp }
+    if ($newDns) { $hostData.interfaces[0].dns = $newDns }
+
+    # Final params object for API call
+    $params = @{
+        host       = $hostData.host
+        name       = $hostData.name
+        status     = $hostData.status
+        macros     = $cleanMacros
+        templates  = $hostData.templates
+        groups     = $hostData.groups
+        interfaces = $hostData.interfaces
+        tags       = $hostData.tags
+    }
+
+    return $params
+}
+
 function Disable-ZXTrigger{
     param(
         [string]$TriggerId,
@@ -4063,6 +4131,47 @@ function Invoke-ZXTask {
 }
 
 
+
+function New-ZXHost {
+    param(
+        [object]$Parameters,
+        [switch]$WhatIf
+    )
+
+    #Basic PS Object wich will be edited based on the used parameters and finally converted to json
+    $PSObj = [PSCustomObject]@{
+        "jsonrpc" = "2.0";
+        "method" = "host.create";
+        "params" = $Parameters
+        "auth" = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR(($Global:ZXAPIToken))); #This is the same as $Global:ZXAPIToken | ConvertFrom-SecureString -AsPlainText but this worsk also for PS 5.1
+        "id" = 1
+    }
+
+    #Convert the PSObjec to Json
+    $Json =  $PSObj | ConvertTo-Json -Depth 5
+    #Show JSON Request if -ShowJsonRequest switch is used
+    If ($ShowJsonRequest -or $WhatIf){
+        Write-Host -ForegroundColor Yellow "JSON REQUEST"
+        $PSObjShow = $PSObj
+        $PSObjShow.auth = "*****"
+        $JsonShow = $PSObjShow | ConvertTo-Json -Depth 5
+        Write-Host -ForegroundColor Cyan $JsonShow
+    }
+
+    #Make the API call
+    if(!$WhatIf){
+        $Request = Invoke-RestMethod -Uri $ZXAPIUrl -Body $Json -ContentType "application/json" -Method Post
+    }
+
+    if($null -ne $Request.error){
+        $Request.error
+        return
+    } 
+    else {
+        return $Request.result
+    }
+    
+}
 
 function New-ZXLogonSession {
     param(
