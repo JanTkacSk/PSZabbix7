6302ef2e9f140289c5ae980bc970d9d4fb1aa59d
JanTkacSK <jan.tkac.sk@gmail.com>
2025-09-11 10:04:21 +0200

    commit

--- Public/Copy-ZXHostParameters.ps1
+++ Public/Copy-ZXHostParameters.ps1
@@ -4,26 +4,26 @@ function Copy-ZXHostParameters {
         [string]$name,
 
         [Parameter(Mandatory=$true)]
-        [string]$sampleHost,
+        [string]$HostTemplate,
 
-        [string]$alias,
-        [string]$newIp,
-        [string]$newDns
+        [string]$NewAlias,
+        [string]$NewIp,
+        [string]$NewDns
     )
 
     # Get host data
-    $hostData = Get-ZXHost -name $sampleHost -IncludeTags -IncludeMacros -IncludeParentTemplates -IncludeInterfaces -IncludeHostGroups -InterfaceProperties extend | ConvertTo-Json -Depth 5 | ConvertFrom-Json -Depth 5
+    $HostData = Get-ZXHost -name $HostTemplate -IncludeTags -IncludeMacros -IncludeParentTemplates -IncludeInterfaces -IncludeHostGroups -InterfaceProperties extend | ConvertTo-Json -Depth 5 | ConvertFrom-Json -Depth 5
 
-    if(!$hostData){
+    if(!$HostData){
         Write-Host -ForegroundColor Yellow "Sample host not found, use a name that exactly matches the host name"
         return
     }
 
     # Remove unwanted properties
-    $hostData.PSObject.Properties.Remove("hostid")
-    $hostData.groups | ForEach-Object { $_.PSObject.Properties.Remove("name") }
-    $hostData.parentTemplates | ForEach-Object { $_.PSObject.Properties.Remove("name") }
-    $hostData.interfaces | ForEach-Object {
+    $HostData.PSObject.Properties.Remove("hostid")
+    $HostData.groups | ForEach-Object { $_.PSObject.Properties.Remove("name") }
+    $HostData.parentTemplates | ForEach-Object { $_.PSObject.Properties.Remove("name") }
+    $HostData.interfaces | ForEach-Object {
         $_.PSObject.Properties.Remove("available")
         $_.PSObject.Properties.Remove("error")
         $_.PSObject.Properties.Remove("errors_from")
@@ -31,36 +31,36 @@ function Copy-ZXHostParameters {
     }
 
     # Clean up macros: keep only macro and value
-    $cleanMacros = @()
-    foreach ($macro in $hostData.macros) {
-        $cleanMacros += @{
-            macro = $macro.macro
-            value = $macro.value
+    $CleanMacros = @()
+    foreach ($macro in $HostData.macros) {
+        $CleanMacros += @{
+            Macro = $Macro.macro
+            value = $Macro.value
         }
     }
 
     # Rename ParentTemplates to templates
-    $hostData | Add-Member -MemberType NoteProperty -Name templates -Value $hostData.parentTemplates
-    $hostData.PSObject.Properties.Remove("parentTemplates")
+    $HostData | Add-Member -MemberType NoteProperty -Name templates -Value $HostData.parentTemplates
+    $HostData.PSObject.Properties.Remove("parentTemplates")
 
     # Replace host and name
-    $hostData.host = $name
-    $hostData.name = if ($alias) { $alias } else { $name }
+    $HostData.host = $name
+    $HostData.name = if ($NewAlias) { $NewAlias } else { $name }
 
     # Update IP and DNS if provided
-    if ($newIp) { $hostData.interfaces[0].ip = $newIp }
-    if ($newDns) { $hostData.interfaces[0].dns = $newDns }
+    if ($NewIp) { $HostData.interfaces[0].ip = $NewIp }
+    if ($NewDns) { $HostData.interfaces[0].dns = $NewDns }
 
     # Final params object for API call
     $params = @{
-        host       = $hostData.host
-        name       = $hostData.name
-        status     = $hostData.status
-        macros     = $cleanMacros
-        templates  = $hostData.templates
-        groups     = $hostData.groups
-        interfaces = $hostData.interfaces
-        tags       = $hostData.tags
+        host       = $HostData.host
+        name       = $HostData.name
+        status     = $HostData.status
+        macros     = $CleanMacros
+        templates  = $HostData.templates
+        groups     = $HostData.groups
+        interfaces = $HostData.interfaces
+        tags       = $HostData.tags
     }
 
     return $params
