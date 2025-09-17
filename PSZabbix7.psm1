7119c821eb58783bf5cc5116c6c9c35b470d0381
JanTkacSK <jan.tkac.sk@gmail.com>
2025-09-17 13:01:13 +0200

    commit

--- PSZabbix7.psm1
+++ PSZabbix7.psm1
@@ -4676,6 +4676,81 @@ function New-ZXTokenSession{
      }
 }
 
+function Remove-ZXDiscoveryRule{
+    param(
+        [array]$LLDRuleID,
+        [switch]$WhatIf,
+        [switch]$ShowJsonRequest,
+        [switch]$ShowJsonResponse
+    )
+   
+    #Funcions
+    #A function that formats and displays the json request that is used in the API call, it removes the API token value and replaces it with *****
+    function ShowJsonRequest {
+        Write-Host -ForegroundColor Yellow "JSON REQUEST"
+        $PSObjShow = $PSObj | ConvertTo-Json -Depth 5 | ConvertFrom-Json -Depth 5
+        $PSObjShow.auth = "*****"
+        $JsonShow = $PSObjShow | ConvertTo-Json -Depth 5 | ConvertFrom-Json -Depth 5
+        Write-Host -ForegroundColor Cyan $JsonShow
+    } 
+
+    #Basic PS Object wich will be edited based on the used parameters and finally converted to json
+    $PSObj  = [PSCustomObject]@{
+        "jsonrpc" = "2.0"; 
+        "method" = "discoveryrule.delete"; 
+        "params" = $LLDRuleID; 
+        "auth" = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR(($Global:ZXAPIToken))); #This is the same as $Global:ZXAPIToken | ConvertFrom-SecureString -AsPlainText but this worsk also for PS 5.1
+        "id" = "1"
+    }
+
+    $ZXDiscoveryRule = Get-ZXDiscoveryRule -ItemID $LLDRuleID
+    if($null -eq $ZXDiscoveryRule.itemid){
+        Write-Host -ForegroundColor Yellow "[Not Found]" -NoNewline
+        Write-Host " $LLDRule"
+        Continue
+    }
+
+    $Json = $PSObj | ConvertTo-Json -Depth 5
+
+
+    #Show JSON Request if -ShowJsonRequest switch is used
+    If ($ShowJsonRequest -or $WhatIf){
+        Write-Host -ForegroundColor Yellow "JSON REQUEST"
+        #Make a clean copy of an object - have to uset convertto and convertfrom in order to break the references to the original object
+        $PSObjShow = $PSObj | ConvertTo-Json | ConvertFrom-Json
+        $PSObjShow.auth = "*****"
+        $JsonShow = $PSObjShow | ConvertTo-Json -Depth 5
+        Write-Host -ForegroundColor Cyan $JsonShow
+    }
+    
+    #Make the API call
+    if(!$Whatif){
+        $Request = Invoke-RestMethod -Uri $ZXAPIUrl -Body $Json -ContentType "application/json" -Method Post
+    }
+
+    If ($ShowJsonResponse){
+        Write-Host -ForegroundColor Yellow "JSON RESPONSE"
+        Write-Host -ForegroundColor Cyan $($request | ConvertTo-Json -Depth 5)
+    }
+
+    #This will be returned by the function
+    if($null -ne $Request.error){
+        $Request.error
+        return
+    } 
+    elseif ($null -ne $Request.result) {
+        $Request.result
+        return
+    }
+    elseif(!$WhatIf) {
+        Write-Host -ForegroundColor Yellow "No result"
+        return
+    }
+    
+}
+
+
+
 function Remove-ZXHost{
     param(
         [array]$HostId,
