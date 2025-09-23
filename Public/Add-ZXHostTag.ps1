99af4e1ab9684d3fe9ccd2ea6b17b25dafc86aa4
JanTkacSK <jan.tkac.sk@gmail.com>
2025-09-23 22:11:19 +0200

    commit

--- Public/Add-ZXHostTag.ps1
+++ Public/Add-ZXHostTag.ps1
@@ -1,14 +1,9 @@
 function Add-ZXHostTag{
     param(
-        $HostName,
-        $HostId,
+        [string]$HostId,
         [string]$TagName, 
         [string]$TagValue,
-        [switch]$WhatIf,
-        [switch]$ShowJsonRequest,
-        [switch]$ShowJsonResponse,
-        [bool]$Continue
-
+        [switch]$WhatIf
     )
 
     #Validate Parameters
@@ -30,47 +25,17 @@ function Add-ZXHostTag{
         }
     }
 
-
-    #Funcions
-    function DateToString{
-        (Get-Date).ToString("2024-MM-dd_HH.mm.ss.ffff")
-    }
-
-    #A function that formats and displays the json request that is used in the API call, it removes the API token value and replaces it with *****
-    function ShowJsonRequest {
-        Write-Host -ForegroundColor Yellow "JSON REQUEST"
-        $PSObjShow = $PSObj | ConvertTo-Json -Depth 5 | ConvertFrom-Json -Depth 5
-        $PSObjShow.auth = "*****"
-        $JsonShow = $PSObjShow | ConvertTo-Json -Depth 5
-        Write-Host -ForegroundColor Cyan $JsonShow
-    }
-    
     #Basic PS Object wich will be edited based on the used parameters and finally converted to json
-    $PSObj  = [PSCustomObject]@{
-        "jsonrpc" = "2.0"; 
-        "method" = "host.update"; 
-        "params" = [PSCustomObject]@{
-            "hostid" = $HostId
-        }; 
-        "auth" = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR(($Global:ZXAPIToken))); #This is the same as $Global:ZXAPIToken | ConvertFrom-SecureString -AsPlainText but this worsk also for PS 5.1
-        "id" = "1"
-    }
-
-    if($HostId){
-        $ZXHost = Get-ZXHost -HostID $HostId -IncludeTags
-    }
-    elseif ($HostName){
-        $ZXHost = Get-ZXHost -Name $HostName -IncludeTags
-    }
+    $PSObj  = New-ZXApiRequestObject -Method "host.update"
+    
+    $ZXHost = Get-ZXHost -HostID $HostId -IncludeTags
 
     if($ZXHost -eq $null){
         Write-Host -ForegroundColor Yellow "Host not found"
         continue
     }
 
-    $PSObj.params.hostid = $ZXHost.hostid
-    $PSObj.params |  Add-Member -MemberType NoteProperty -Name "host" -Value $ZXHost.host
-    $PSObj.params |  Add-Member -MemberType NoteProperty -Name "name" -Value $ZXHost.name
+    $PSObj.params | Add-Member -MemberType NoteProperty -Name "hostid" $HostId
     
     #Get the list of host tags.
     [System.Collections.ArrayList]$TagList = $ZXHost.tags
@@ -79,40 +44,17 @@ function Add-ZXHostTag{
 
     $PSObj.params |  Add-Member -MemberType NoteProperty -Name "tags" -Value @($TagList)
 
-    
     $Json = $PSObj | ConvertTo-Json -Depth 5
 
-    #Show JSON Request if -ShowJsonRequest switch is used
-    If ($ShowJsonRequest -or $WhatIf){
-        Write-Host -ForegroundColor Yellow "JSON REQUEST"
-        $PSObjShow = $PSObj | ConvertTo-Json -Depth 5 | ConvertFrom-Json -Depth 5
-        $PSObjShow.auth = "*****"
-        $JsonShow = $PSObjShow | ConvertTo-Json -Depth 5
-        Write-Host -ForegroundColor Cyan $JsonShow
+    #Show JSON Request if -WhatIf switch is used
+    If ($WhatIf){
+        Write-JsonRequest
     }
     
     #Make the API call
     if(!$Whatif){
         $Request = Invoke-RestMethod -Uri $ZXAPIUrl -Body $Json -ContentType "application/json" -Method Post
-    }
-
-    If ($ShowJsonResponse){
-        Write-Host -ForegroundColor Yellow "JSON RESPONSE"
-        Write-Host -ForegroundColor Cyan $($Request | ConvertTo-Json -Depth 5)
-    }
-
-    #This will be returned by the function
-    if($null -ne $Request.error){
-        $Request.error
-        return
-    }
-    elseif($null -ne $Request.result){
-        $Request.result
-        return
-    }
-    elseif(!$WhatIf) {
-        Write-Host -ForegroundColor Yellow "No result"
-        return
+        Resolve-ZXApiResponse -Request $Request
     }
     
 }
--- Public/Class-TagList.ps1
+++ Public/Class-TagList.ps1
@@ -1,22 +1,3 @@
-<# 
-class ZXTagList {
-    [System.Collections.ArrayList]$Tags
-
-    ZXTagList() {
-        $this.Tags = [System.Collections.ArrayList]::new()
-    }
-
-    [void] AddTag([string]$name, [string]$value) {
-        $newObject = [PSCustomObject]@{ Name = $name; Value = $value }
-        $this.Tags.Add($newObject) | Out-Null
-    }
-
-    [void] Display() {
-        $this.Tags
-    }
-}
-#>
-
 class ZXTagList {
     [System.Collections.ArrayList]$Tags
 
