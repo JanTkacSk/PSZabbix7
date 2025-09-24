3df4c74b45936760f9064e26ebb2b209360c8945
JanTkacSK <jan.tkac.sk@gmail.com>
2025-09-24 22:28:21 +0200

    commit

--- Public/Get-ZXHostGroup.ps1
+++ Public/Get-ZXHostGroup.ps1
@@ -7,19 +7,9 @@ function Get-ZXHostGroup {
         [array]$GroupID,
         [switch]$WithHosts,
         [switch]$WithMonitoredItems,
-        [switch]$ShowJsonRequest,
-        [switch]$ShowJsonResponse,
-        [switch]$WhatIf
+        [switch]$WhatIf,
+        [int]$Limit
     )
-
-    #A function that formats and displays the json request that is used in the API call, it removes the API token value and replaces it with *****
-    function ShowJsonRequest {
-        Write-Host -ForegroundColor Yellow "JSON REQUEST"
-        $PSObjShow = $PSObj
-        $PSObjShow.auth = "*****"
-        $JsonShow = $PSObjShow | ConvertTo-Json -Depth 5
-        Write-Host -ForegroundColor Cyan $JsonShow
-    }
 
     if (!$Output){
         $Output = @("name","groupid")
@@ -30,14 +20,7 @@ function Get-ZXHostGroup {
     
 
     #Basic PS Object wich will be edited based on the used parameters and finally converted to json
-    $PSObj = [PSCustomObject]@{
-        "jsonrpc" = "2.0";
-        "method" = "hostgroup.get";
-        "params" = [PSCustomObject]@{
-        };
-        "auth" = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR(($Global:ZXAPIToken))); #This is the same as $Global:ZXAPIToken | ConvertFrom-SecureString -AsPlainText but this worsk also for PS 5.1
-        "id" = 1
-    }
+    $PSObj = New-ZXApiRequestObject -Method "hostgroup.get"
 
     # Add "selectHosts" parameter to return all hosts linked tho the templates.
     if ($IncludeHosts){
@@ -56,14 +39,15 @@ function Get-ZXHostGroup {
         $PSObj.params | Add-Member -MemberType NoteProperty -Name "output" -Value $Output
     }
 
+    #Limit the number of returned Groups
+    if($Limit){
+        $PSObj.params | Add-Member -MemberType NoteProperty -Name "limit" -Value $Limit
+    }
 
-    #Show JSON Request if -ShowJsonRequest switch is used
-    If ($ShowJsonRequest){
-        Write-Host -ForegroundColor Yellow "JSON REQUEST"
-        $PSObjShow = $PSObj | ConvertTo-Json | ConvertFrom-Json
-        $PSObjShow.auth = "*****"
-        $JsonShow = $PSObjShow | ConvertTo-Json -Depth 5
-        Write-Host -ForegroundColor Cyan $JsonShow
+
+    #Show JSON Request if -WhatIf switch is used
+    If ($WhatIf){
+        Write-JsonRequest
     }
 
     $JSON = $PSObj | ConvertTo-Json -Depth 5
@@ -71,19 +55,6 @@ function Get-ZXHostGroup {
     #Make the API call
     if(!$WhatIf){
         $Request = Invoke-RestMethod -Uri $ZXAPIUrl -Body $Json -ContentType "application/json" -Method Post
-    }
-
-    If ($ShowJsonResponse){
-        Write-Host -ForegroundColor Yellow "JSON RESPONSE"
-        Write-Host -ForegroundColor Cyan $($request.result | ConvertTo-Json -Depth 5)
-    }
-  
-    if($null -ne $Request.error){
-        $Request.error
-        return
-    } 
-    else {
-        $Request.result
-        return
+        Resolve-ZXApiResponse -Request $Request
     }
 }
--- Public/Get-ZXHostInterface.ps1
+++ Public/Get-ZXHostInterface.ps1
@@ -6,8 +6,7 @@ function Get-ZXHostInterface {
         [array]$HostID,
         [array]$ItemProperties,
         [switch]$IncludeItems,
-        [switch]$ShowJsonRequest,
-        [switch]$ShowJsonResponse,
+        [int]$Limit,
         [switch]$WhatIf
 
     )
@@ -22,23 +21,7 @@ function Get-ZXHostInterface {
     }
    
     #Basic PS Object wich will be edited based on the used parameters and finally converted to json
-    $PSObj = [PSCustomObject]@{
-        "jsonrpc" = "2.0";
-        "method" = "hostinterface.get";
-        "params" = [PSCustomObject]@{
-        };
-        "auth" = [Runtime.InteropServices.Marshal]::PtrToStringAuto([Runtime.InteropServices.Marshal]::SecureStringToBSTR(($Global:ZXAPIToken))); #This is the same as $Global:ZXAPIToken | ConvertFrom-SecureString -AsPlainText but this worsk also for PS 5.1
-        "id" = 1
-    }
-    
-    #A function that formats and displays the json request that is used in the API call, it removes the API token value and replaces it with *****
-    function ShowJsonRequest {
-        Write-Host -ForegroundColor Yellow "JSON REQUEST"
-        $PSObjShow = $PSObj
-        $PSObjShow.auth = "*****"
-        $JsonShow = $PSObjShow | ConvertTo-Json -Depth 5
-        Write-Host -ForegroundColor Cyan $JsonShow
-    }
+    $PSObj = New-ZXApiRequestObject -Method "hostinterface.get"
     
     if($IP){AddFilter -PropertyName "ip" -PropertyValue $IP}
     if($IPSearch){AddSearch -PropertyName "ip" -PropertyValue $IPSearch}
@@ -52,35 +35,23 @@ function Get-ZXHostInterface {
         $PSObj.params | Add-Member -MemberType NoteProperty -Name "selectItems" -Value $ItemProperties
     }
 
+    #Limit the number of returned Interfaces
+    if($Limit){
+        $PSObj.params | Add-Member -MemberType NoteProperty -Name "limit" -Value $Limit
+    }
+
     #$PSObj.params.output = "extend"
     $Json =  $PSObj | ConvertTo-Json -Depth 3
 
-    #Show JSON Request based on the -ShowJsonRequest switch.
-    If ($ShowJsonRequest -or $WhatIf){
-        Write-Host -ForegroundColor Yellow "JSON REQUEST"
-        $PSObjShow = $PSObj
-        $PSObjShow.auth = "*****"
-        $JsonShow = $PSObjShow | ConvertTo-Json -Depth 5
-        Write-Host -ForegroundColor Cyan $JsonShow
+    if ($WhatIf){
+        Write-JsonRequest
     }
+
 
     #Make the API call
     if(!$WhatIf){
         $Request = Invoke-RestMethod -Uri $ZXAPIUrl -Body $Json -ContentType "application/json" -Method Post
+        Resolve-ZXApiResponse -Request $Request
     }
 
-    If ($ShowJsonResponse){
-        Write-Host -ForegroundColor Yellow "JSON RESPONSE"
-        Write-Host -ForegroundColor Cyan $($request.result | ConvertTo-Json -Depth 5)
-    }
-
-    #This will be returned by the function
-    if($null -ne $Request.error){
-        $Request.error
-        return
-    } 
-    else {
-        $Request.result
-        return
-    }
 }
