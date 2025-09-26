089d76084fd95d29f531fe449c42a1bca993aa6e
JanTkacSK <jan.tkac.sk@gmail.com>
2025-09-26 10:48:39 +0200

    commit

--- Private/AddFunctionsToPSM.ps1
+++ Private/AddFunctionsToPSM.ps1
@@ -15,5 +15,58 @@ foreach ($Function in $FunctionFiles){
     $ModuleFileContent += "`n" + $FunctionContent
 }
 
+$ModuleFileContent += 'Export-ModuleMember -Function `
+    Add-ZXHostGroup, `
+    Add-ZXHostNameSuffix, `
+    Add-ZXHostTag, `
+    Copy-ZXHostParameters, `
+    Disable-ZXTrigger, `
+    Enable-ZXTrigger, `
+    Get-ZXAction, `
+    Get-ZXAlert, `
+    Get-ZXApiVersion, `
+    Get-ZXAuditLog, `
+    Get-ZXDiscoveryRule, `
+    Get-ZXEvent, `
+    Get-ZXHistory, `
+    Get-ZXHost, `
+    Get-ZXHostGroup, `
+    Get-ZXHostInterface, `
+    Get-ZXItem, `
+    Get-ZXItemPrototype, `
+    Get-ZXMaintenance, `
+    Get-ZXProblem, `
+    Get-ZXProxy, `
+    Get-ZXService, `
+    GEt-ZXSession, `
+    Get-ZXTemplate, `
+    Get-ZXTrigger, `
+    Get-ZXTriggerPrototype, `
+    Invoke-ZXTask, `
+    New-ZXHost, `
+    New-ZXProblemTagList, `
+    New-ZXService, `
+    New-ZXTagFilter, `
+    New-ZXTagList, `
+    New-ZXTokenSession, `
+    Remove-ZXDiscoveryRule, `
+    Remove-ZXHost, `
+    Remove-ZXHostGroup, `
+    Remove-ZXHostNameSuffix, `
+    Remove-ZXHostTag, `
+    Remove-ZXItem, `
+    Remove-ZXMaintenance, `
+    Remove-ZXTrigger, `
+    Remove-ZXTriggerPrototype, `
+    Set-ZXHostLetterCase, `
+    Set-ZXHostName, `
+    Set-ZXHostStatus, `
+    Stop-ZXSession, `
+    Update-ZXHostTagList, `
+    Update-ZXHostTemplateList, `
+    Update-ZXMaintenance, `
+    Update-ZXService'
+
+
 $ModuleFileContent | Out-File $ModuleFile -Force
 
